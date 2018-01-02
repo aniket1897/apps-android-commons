5c3e7844dd8a4bddf3ede0c98beeb0c91abe2a94
misaochan <josephinelim86@gmail.com>
2018-01-02 21:33:10 +1000

    Pass nearby Title and Desc to SingleUploadFragment

--- app/src/main/java/fr/free/nrw/commons/upload/SingleUploadFragment.java
+++ app/src/main/java/fr/free/nrw/commons/upload/SingleUploadFragment.java
@@ -112,8 +112,10 @@ public class SingleUploadFragment extends CommonsDaggerSupportFragment {
 
         //TODO: Get this to display title and desc
         if (true) {
-            titleEdit.setText("");
-            descEdit.setText("");
+            String imageTitle = ((ShareActivity) getActivity()).getNearbyTitle();
+            String imageDesc = ((ShareActivity) getActivity()).getNearbyDesc();
+            titleEdit.setText(imageTitle);
+            descEdit.setText(imageDesc);
         }
 
         // check if this is the first time we have uploaded
@@ -275,7 +277,7 @@ public class SingleUploadFragment extends CommonsDaggerSupportFragment {
         }
         return false;
     }
-
+    
     private void setLicenseSummary(String license) {
         licenseSummaryView.setText(getString(R.string.share_license_summary, getString(Utils.licenseNameFor(license))));
     }
